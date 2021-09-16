7536d4e0b0bcc6c304f1977e0ecc8340e89b0730
Evin Sellin <evin@vidora.com>
2021-09-16 14:56:57 -0700

    add rollup to estimate bundle size. fpjs is gigantic

---
+++ js/rollup.config.js
@@ -0,0 +1,13 @@
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: 'dist/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'umd',
+    name: "millieql"
+  },
+  plugins: [nodeResolve(), commonjs(), terser()]
+};
