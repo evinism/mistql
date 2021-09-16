4106b19073c8c62a038350033f072290af3261ad
Evin Sellin <evin@vidora.com>
2021-09-16 15:08:51 -0700

    change index to function as intended

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,2 +1,8 @@
-export { parse } from './parser';
+import { execute } from './executor';
+import { parseOrThrow } from './parser';
 
+export const query = (query: string, data: any) => {
+  return execute(parseOrThrow(query), data);
+}
+
+export default { query };
