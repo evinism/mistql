b67d8718a3100f69a2d98b9339059001c3e77026
Evin Sellin <evinism@gmail.com>
2021-09-17 23:52:56 -0700

    millie -> beaker

--- js/rollup.config.js
+++ js/rollup.config.js
@@ -1,14 +1,14 @@
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import analyze from 'rollup-plugin-analyzer';
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import analyze from "rollup-plugin-analyzer";
 import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: 'dist/index.js',
+  input: "dist/index.js",
   output: {
-    file: 'dist/bundle.js',
-    format: 'umd',
-    name: "millieql"
+    file: "dist/bundle.js",
+    format: "umd",
+    name: "beakerql",
   },
-  plugins: [nodeResolve(), commonjs(), terser(), analyze()]
+  plugins: [nodeResolve(), commonjs(), terser(), analyze()],
 };
