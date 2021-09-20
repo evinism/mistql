96990a1f589d2ad6a997b68053ee9526c492b8d8
Evin Sellin <evinism@gmail.com>
2021-09-19 17:31:15 -0700

    add summary

--- js/src/builtins.ts
+++ js/src/builtins.ts
@@ -312,30 +312,67 @@ const log: BuiltinFunction = arity(1, (args, stack, exec) => {
   return res;
 });
 
+// For Summary
+const _getMean = (array: number[]) => {
+  return array.reduce((a, b) => a + b, 0) / array.length;
+}
+
+const _getMedian = (array: number[]) => {
+  const midpoint = array.length / 2;
+  const median = midpoint % 1 ? array[midpoint - 0.5] : (array[midpoint - 1] + array[midpoint]) / 2;
+  return median;
+}
+
+const _getVariance = (array: number[]) => {
+  var mean = _getMean(array);
+  return _getMean(array.map(function(num) {
+    return Math.pow(num - mean, 2);
+  }));
+};
+
+const _getStandardDeviation = (array: number[]) => Math.sqrt(_getVariance(array));
+
+const summarize: BuiltinFunction = arity(1, (args, stack, exec) => {
+  const array = validateType("array", exec(args[0], stack));
+  array.sort((a, b) => a - b);
+  array.forEach(value => validateType("number", value));
+
+  return {
+    max: Math.max.apply(null, array),
+    min: Math.min.apply(null, array),
+    mean: _getMean(array),
+    median: _getMedian(array),
+    variance: _getVariance(array),
+    stddev: _getStandardDeviation(array),
+  };
+});
+
+
 export default {
-  if: ifFunction,
-  map,
-  filter,
-  reduce,
-  mapvalues,
-  filtervalues,
   //mapkeys,
-  filterkeys,
-  find,
   count,
-  keys,
-  values,
-  sum,
-  groupby,
-  index,
+  filter,
+  filterkeys,
+  filtervalues,
+  find,
   first,
+  groupby,
+  head,
+  if: ifFunction,
+  index,
+  keys,
   last,
+  log,
+  map,
+  mapvalues,
+  reduce,
+  reverse,
   sort,
   sortby,
-  reverse,
-  head,
+  sum,
+  summarize,
   tail,
-  log,
+  values,
   "!/unary": notOp,
   "-/unary": unaryMinus,
   ".": dotAccessor,
