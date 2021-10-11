b5049a380d8abaca1f0f4ef9458d5f1bb703edd3
Evin Sellin <evin@vidora.com>
2021-10-11 16:10:23 -0700

    allow using data hashes to pass values to try it out

--- docs/src/components/TryItOut.js
+++ docs/src/components/TryItOut.js
@@ -1,9 +1,8 @@
-import React, {useState} from 'react';
-import styles from './TryItOut.module.css';
 import mistql from 'mistql';
+import React, { useState } from 'react';
 import examples from '../examples';
+import styles from './TryItOut.module.css';
 
-const defaultExample = examples['usersWithAMessageEvent'];
 
 const runQuery = (query, data) => {
   try {
@@ -19,44 +18,56 @@ const runQuery = (query, data) => {
   }
 }
 
+const getDefaultExample = () => {
+  if (location.hash.startsWith('#data:')) {
+    return {
+      name: 'custom',
+      query: '@',
+      data: JSON.parse(atob(location.hash.substr(6)))
+    }
+  }
+  return examples['usersWithAMessageEvent'];
+}
+
 export default function TryItOut() {
+  const defaultExample = getDefaultExample();
   const [dataText, setDataText] = useState(JSON.stringify(defaultExample.data, null, 2));
   const [queryText, setQueryText] = useState(defaultExample.query);
-  const {success, result, error} = runQuery(queryText, dataText);
+  const { success, result, error } = runQuery(queryText, dataText);
   return (
     <section className={styles.tryitout}>
       <div className={styles.tryitoutinner}>
-      <h2>Try it out!</h2>
+        <h2>Try it out!</h2>
 
-      <div className={styles.querywrapper}>
-        <div className={styles.labelblock}>
-          <label for="tryitoutquery">MistQL Query</label>
-          <label>
-            Example
+        <div className={styles.querywrapper}>
+          <div className={styles.labelblock}>
+            <label for="tryitoutquery">MistQL Query</label>
+            <label>
+              Example
             <select onChange={(e) => {
-              const {data, query} = examples[e.target.value]
-              setDataText(JSON.stringify(data, null, 2));
-              setQueryText(query)
-            }}>
-              {Object.entries(examples).map(([value, example]) => <option key={value} value={value}>{example.name}</option>)}
-            </select>
-          </label>
+                const { data, query } = examples[e.target.value]
+                setDataText(JSON.stringify(data, null, 2));
+                setQueryText(query)
+              }}>
+                {Object.entries(examples).map(([value, example]) => <option key={value} value={value}>{example.name}</option>)}
+              </select>
+            </label>
+          </div>
+          <input id="tryitoutquery" className={styles.queryinput} type="text" value={queryText} onChange={(e) => setQueryText(e.target.value)} />
         </div>
-        <input id="tryitoutquery" className={styles.queryinput} type="text" value={queryText} onChange={(e) => setQueryText(e.target.value)} />
-      </div>
-      <div className={styles.datawrapper}>
-        <div styles={styles.labelblock}>
-          <label for="tryitoutdata">JSON Data</label>
+        <div className={styles.datawrapper}>
+          <div styles={styles.labelblock}>
+            <label for="tryitoutdata">JSON Data</label>
+          </div>
+          <textarea id="tryitoutdata" className={styles.datafield} value={dataText} onChange={(e) => setDataText(e.target.value)}>
+          </textarea>
         </div>
-        <textarea id="tryitoutdata" className={styles.datafield} value={dataText} onChange={(e) => setDataText(e.target.value)}>
-        </textarea>
-      </div>
-      <div className={styles.resultwrapper}>
-        Result:
+        <div className={styles.resultwrapper}>
+          Result:
         <pre>
-          {success ? JSON.stringify(result, null, 2) : error.message}
-        </pre>
-      </div>
+            {success ? JSON.stringify(result, null, 2) : error.message}
+          </pre>
+        </div>
       </div>
     </section>
   );
