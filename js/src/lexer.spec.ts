906e0f7414aab860266e3542b555b1a6e7831a51
Evin Sellin <evin@vidora.com>
2021-10-13 15:23:02 -0700

    make token positions work well

--- js/src/lexer.spec.ts
+++ js/src/lexer.spec.ts
@@ -5,37 +5,37 @@ describe("lexer", () => {
   describe("#lex", () => {
     it("should lex a basic expression", () => {
       assert.deepStrictEqual(lex("hello + there"), [
-        { token: "ref", value: "hello" },
-        { token: "special", value: "+" },
-        { token: "ref", value: "there" },
+        { token: "ref", value: "hello", position: 0 },
+        { token: "special", value: "+", position: 6 },
+        { token: "ref", value: "there", position: 8 },
       ]);
     });
 
     it("should lex an expression with an initially ambiguous operator", () => {
       assert.deepStrictEqual(lex("hello || there"), [
-        { token: "ref", value: "hello" },
-        { token: "special", value: "||" },
-        { token: "ref", value: "there" },
+        { token: "ref", value: "hello", position: 0 },
+        { token: "special", value: "||", position: 6 },
+        { token: "ref", value: "there", position: 9 },
       ]);
     });
     it("should lex a string", () => {
       assert.deepStrictEqual(lex('"sup" && there'), [
-        { token: "value", value: "sup" },
-        { token: "special", value: "&&" },
-        { token: "ref", value: "there" },
+        { token: "value", value: "sup", position: 0 },
+        { token: "special", value: "&&", position: 6 },
+        { token: "ref", value: "there", position: 9 },
       ]);
     });
 
     it("handles escaped strings", () => {
       assert.deepStrictEqual(lex('"sup\\"" && there'), [
-        { token: "value", value: 'sup"' },
-        { token: "special", value: "&&" },
-        { token: "ref", value: "there" },
+        { token: "value", value: 'sup"', position: 0 },
+        { token: "special", value: "&&", position: 8 },
+        { token: "ref", value: "there", position: 11 },
       ]);
       assert.deepStrictEqual(lex('"sup\\\\"   there'), [
-        { token: "value", value: "sup\\" },
-        { token: "special", value: " " },
-        { token: "ref", value: "there" },
+        { token: "value", value: "sup\\", position: 0 },
+        { token: "special", value: " ", position: 7 },
+        { token: "ref", value: "there", position: 10 },
       ]);
     });
 
@@ -45,7 +45,7 @@ describe("lexer", () => {
 
     it("trims whitespace", () => {
       assert.deepStrictEqual(lex('  @  '), [
-        { token: "ref", value: "@" },
+        { token: "ref", value: "@", position: 2 },
       ]);
     });
 
@@ -64,33 +64,33 @@ describe("lexer", () => {
         '@  \n   \t  \t\t\t@',
       ];
       cases.forEach((item) => assert.deepStrictEqual(lex(item), [
-        { token: "ref", value: '@' },
-        { token: "special", value: " " },
-        { token: "ref", value: '@' },
+        { token: "ref", value: '@', position: 0 },
+        { token: "special", value: " ", position: 1 },
+        { token: "ref", value: '@', position: item.length - 1 },
       ]));
     })
 
     it("should handle right, left, and rl-binding tokens", () => {
       assert.deepStrictEqual(lex("  +  "), [
-        { token: "special", value: "+" },
+        { token: "special", value: "+", position: 2 },
       ]);
 
       ['(', '{', '['].forEach((token) => {
         assert.deepStrictEqual(lex(`@  ${token}  @`), [
-          { token: "ref", value: '@' },
-          { token: "special", value: " " },
-          { token: "special", value: token },
-          { token: "ref", value: '@' },
+          { token: "ref", value: '@', position: 0 },
+          { token: "special", value: " ", position: 1 },
+          { token: "special", value: token, position: 3 },
+          { token: "ref", value: '@', position: 6 },
 
         ]);
       });
 
       [')', '}', ']'].forEach((token) => {
         assert.deepStrictEqual(lex(`@  ${token}  @`), [
-          { token: "ref", value: '@' },
-          { token: "special", value: token },
-          { token: "special", value: " " },
-          { token: "ref", value: '@' },
+          { token: "ref", value: '@', position: 0 },
+          { token: "special", value: token, position: 3 },
+          { token: "special", value: " ", position: 4 },
+          { token: "ref", value: '@', position: 6 },
         ]);
       });
     });
