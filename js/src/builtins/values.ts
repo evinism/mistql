1a7dcead8a016584eb43f07684972b3f7bd76fd8
Evin Sellin <evinism@gmail.com>
2021-10-16 02:15:27 -0700

    unify a few disparate definitions

--- js/src/builtins/values.ts
+++ js/src/builtins/values.ts
@@ -1,15 +1,10 @@
+import { getProperties } from "../runtimeValues";
 import { BuiltinFunction } from "../types";
 import { arity } from "../util";
 
 const values: BuiltinFunction = arity(1, (args, stack, exec) => {
   const evaluated = exec(args[0], stack);
-  const results = [];
-  for (let i in evaluated) {
-    if (evaluated.hasOwnProperty(i)) {
-      results.push(evaluated[i]);
-    }
-  }
-  return results;
+  return getProperties(evaluated).map((key) => evaluated[key]);
 });
 
 export default values;
