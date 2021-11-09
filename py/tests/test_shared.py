299f280a01296ec89f0bf1683568818b68a086d4
Evin Sellin <evin@vidora.com>
2021-11-09 14:19:46 -0800

    allow shared tests

--- py/tests/test_shared.py
+++ py/tests/test_shared.py
@@ -8,24 +8,26 @@ with open("shared/testdata.json") as f:
     testdata = json.load(f)
 
 
-Testcase = Tuple[List[Tuple[str, Any, Any]], str, str]
+Testcase = Tuple[List[Tuple[str, Any, Any]], str, str, str]
 testcases: List[Testcase] = []
 
 for block in testdata["data"]:
-    for test in block["cases"]:
-        testcases.append(
-            (
-                [
-                    (assertion["query"], assertion["data"], assertion["expected"])
-                    for assertion in test["assertions"]
-                ],
-                block["describe"],
-                test["it"],
+    for innerblock in block["cases"]:
+        for test in innerblock["cases"]:
+            testcases.append(
+                (
+                    [
+                        (assertion["query"], assertion["data"], assertion["expected"])
+                        for assertion in test["assertions"]
+                    ],
+                    block["describe"],
+                    innerblock["describe"],
+                    test["it"],
+                )
             )
-        )
 
 
-@pytest.mark.parametrize("testcase", testcases, ids=lambda x: f"{x[1]}::{x[2]}")
+@pytest.mark.parametrize("testcase", testcases, ids=lambda x: f"{x[1]}::{x[2]}::{x[3]}")
 def test_shared(testcase: Testcase):
     for target_query, data, expected in testcase[0]:
         assert query(target_query, data) == expected
