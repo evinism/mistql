1daf053263bdd5fe21f477c2bc6e8dbd09713ecb
Evin Sellin <evinism@gmail.com>
2021-11-15 17:59:16 -0800

    Introduce lark parser as mistql parser for python

--- py/mistql/parse.py
+++ py/mistql/parse.py
@@ -1,2 +1,47 @@
+from lark import Lark
+
+mistql_parser = Lark(r"""
+?start: expression
+
+?expression: sum | literal
+
+?literal: object
+      | array
+      | string
+      | SIGNED_NUMBER      -> number
+      | "true"             -> true
+      | "false"            -> false
+      | "null"             -> null
+
+array  : "[" [literal ("," literal)*] "]"
+object : "{" [pair ("," pair)*] "}"
+pair   : string ":" literal
+string : ESCAPED_STRING
+
+?sum: product
+    | sum "+" product   -> add
+    | sum "-" product   -> sub
+
+?product: atom
+    | product "*" atom  -> mul
+    | product "/" atom  -> div
+
+?atom: NUMBER           -> number
+     | "-" atom         -> neg
+     | "(" expression ")"
+     | literal
+
+%import common.ESCAPED_STRING
+%import common.SIGNED_NUMBER
+%import common.WS
+%import common.NUMBER
+
+%ignore WS
+
+
+""", parser="earley")
+
 def parse(raw):
-    raise NotImplementedError("parse() not implemented")
+    parsed = mistql_parser.parse(raw)
+    print(parsed.pretty())
+    return parsed
