e1c548704ea61a1ce83d79365565cd59f7b2b974
Evin Sellin <evinism@gmail.com>
2022-01-04 22:26:19 -0800

    equality and booleans

--- py/mistql/builtins.py
+++ py/mistql/builtins.py
@@ -36,9 +36,31 @@ def unary_minus(arguments: Args, stack: Stack, execute: Exec) -> RuntimeValue:
         raise Exception(f"unary_minus takes a number, got {res}")
     return RuntimeValue.of(-res.value)
 
+def unary_not(arguments: Args, stack: Stack, execute: Exec) -> RuntimeValue:
+    if len(arguments) != 1:
+        raise Exception("unary not takes one argument")
+    res = execute(arguments[0], stack)
+    return RuntimeValue.of(not res.truthy())
+
+def eq(arguments: Args, stack: Stack, execute: Exec) -> RuntimeValue:
+    if len(arguments) != 2:
+        raise Exception("eq takes two arguments")
+    left = execute(arguments[0], stack)
+    right = execute(arguments[1], stack)
+    return RuntimeValue.of(RuntimeValue.eq(left, right))
+
+def neq(arguments: Args, stack: Stack, execute: Exec) -> RuntimeValue:
+    if len(arguments) != 2:
+        raise Exception("neq takes two arguments")
+    left = execute(arguments[0], stack)
+    right = execute(arguments[1], stack)
+    return RuntimeValue.of(not RuntimeValue.eq(left, right).value)
 
 builtins = {
     "-/unary": unary_minus,
+    "!/unary": unary_not,
+    "==": eq,
+    "!=": neq,
     "reverse": reverse,
     "log": log,
 }
