8eecffec3d0aec06d965553e9468945c8df388f5
Evin Sellin <evin@vidora.com>
2022-01-04 17:09:29 -0800

    lots of incremental progress

--- py/mistql/gardenwall.py
+++ py/mistql/gardenwall.py
@@ -1,11 +1,10 @@
 from typing import Any
+from mistql.runtime_value import RuntimeValue
 
 
-def input_garden_wall(data: Any) -> Any:
-    return data # This is very wrong.
-    # raise NotImplementedError("input_garden_wall() not yet implemented")
+def input_garden_wall(data: Any) -> RuntimeValue:
+    return RuntimeValue.of(data)
 
 
-def output_garden_wall(data: Any) -> Any:
-    return data # This is very wrong.
-    # raise NotImplementedError("input_garden_wall() not yet implemented")
+def output_garden_wall(data: RuntimeValue) -> Any:
+    return data.to_python()
