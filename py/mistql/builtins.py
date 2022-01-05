25cc1bfac778ac931fd1b87e6e8f48ad559fc220
Evin Sellin <evinism@gmail.com>
2022-01-04 22:35:41 -0800

    more builtins

--- py/mistql/builtins.py
+++ py/mistql/builtins.py
@@ -42,6 +42,66 @@ def unary_not(arguments: Args, stack: Stack, execute: Exec) -> RuntimeValue:
     res = execute(arguments[0], stack)
     return RuntimeValue.of(not res.truthy())
 
+def if_else(arguments: Args, stack: Stack, execute: Exec) -> RuntimeValue:
+    if len(arguments) != 3:
+        raise Exception("if takes three arguments")
+    condition = execute(arguments[0], stack)
+    if condition.truthy():
+        return execute(arguments[1], stack)
+    else:
+        return execute(arguments[2], stack)
+
+def add(arguments: Args, stack: Stack, execute: Exec) -> RuntimeValue:
+    if len(arguments) != 2:
+        raise Exception("add takes two arguments")
+    left = execute(arguments[0], stack)
+    right = execute(arguments[1], stack)
+    if left.type != right.type:
+        raise Exception(f"add: {left} and {right} are not the same type")
+    if left.type == RuntimeValueType.Number:
+        return RuntimeValue.of(left.value + right.value)
+    if left.type == RuntimeValueType.String:
+        return RuntimeValue.of(left.value + right.value)
+    if left.type == RuntimeValueType.Array:
+        return RuntimeValue.of(left.value + right.value)
+    raise Exception(f"add: {left.type} is not supported")
+
+def subtract(arguments: Args, stack: Stack, execute: Exec) -> RuntimeValue:
+    if len(arguments) != 2:
+        raise Exception("subtract takes two arguments")
+    left = execute(arguments[0], stack)
+    right = execute(arguments[1], stack)
+    if left.type != RuntimeValueType.Number or right.type != RuntimeValueType.Number:
+        raise Exception(f"subtract: {left} and {right} are not both numbers")
+    return RuntimeValue.of(left.value - right.value)
+
+def multiply(arguments: Args, stack: Stack, execute: Exec) -> RuntimeValue:
+    if len(arguments) != 2:
+        raise Exception("multiply takes two arguments")
+    left = execute(arguments[0], stack)
+    right = execute(arguments[1], stack)
+    if left.type != RuntimeValueType.Number or right.type != RuntimeValueType.Number:
+        raise Exception(f"multiply: {left} and {right} are not both numbers")
+    return RuntimeValue.of(left.value * right.value)
+
+def divide(arguments: Args, stack: Stack, execute: Exec) -> RuntimeValue:
+    if len(arguments) != 2:
+        raise Exception("divide takes two arguments")
+    left = execute(arguments[0], stack)
+    right = execute(arguments[1], stack)
+    if left.type != RuntimeValueType.Number or right.type != RuntimeValueType.Number:
+        raise Exception(f"divide: {left} and {right} are not both numbers")
+    return RuntimeValue.of(left.value / right.value)
+
+def mod(arguments: Args, stack: Stack, execute: Exec) -> RuntimeValue:
+    if len(arguments) != 2:
+        raise Exception("mod takes two arguments")
+    left = execute(arguments[0], stack)
+    right = execute(arguments[1], stack)
+    if left.type != RuntimeValueType.Number or right.type != RuntimeValueType.Number:
+        raise Exception(f"mod: {left} and {right} are not both numbers")
+    return RuntimeValue.of(left.value % right.value)
+
 def eq(arguments: Args, stack: Stack, execute: Exec) -> RuntimeValue:
     if len(arguments) != 2:
         raise Exception("eq takes two arguments")
@@ -56,11 +116,39 @@ def neq(arguments: Args, stack: Stack, execute: Exec) -> RuntimeValue:
     right = execute(arguments[1], stack)
     return RuntimeValue.of(not RuntimeValue.eq(left, right).value)
 
+def and_fn(arguments: Args, stack: Stack, execute: Exec) -> RuntimeValue:
+    if len(arguments) != 2:
+        raise Exception("and takes two arguments")
+    left = execute(arguments[0], stack)
+    right = execute(arguments[1], stack)
+    if left.truthy():
+        return right
+    else:
+        return left
+
+def or_fn(arguments: Args, stack: Stack, execute: Exec) -> RuntimeValue:
+    if len(arguments) != 2:
+        raise Exception("or takes two arguments")
+    left = execute(arguments[0], stack)
+    right = execute(arguments[1], stack)
+    if left.truthy():
+        return left
+    else:
+        return right
+
 builtins = {
     "-/unary": unary_minus,
     "!/unary": unary_not,
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+    "%": mod,
     "==": eq,
     "!=": neq,
+    "&&": and_fn,
+    "||": or_fn,
+    "if": if_else,
     "reverse": reverse,
     "log": log,
 }
