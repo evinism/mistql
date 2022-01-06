3d80a5630189591e9bbf34a0ba1461319eb342d9
Evin Sellin <evin@vidora.com>
2022-01-06 13:10:18 -0800

    correct splits

--- py/tests/test_shared.py
+++ py/tests/test_shared.py
@@ -8,13 +8,13 @@ with open("shared/testdata.json") as f:
     testdata = json.load(f)
 
 
-Testcase = Tuple[List[Tuple[str, Any, Any]], str, str, str]
-testcases: List[Testcase] = []
+Case = Tuple[List[Tuple[str, Any, Any]], str, str, str]
+cases: List[Tuple[List[Tuple[str, Any, Any]], str, str, str]] = []
 
 for block in testdata["data"]:
     for innerblock in block["cases"]:
         for test in innerblock["cases"]:
-            testcases.append(
+            cases.append(
                 (
                     [
                         (assertion["query"], assertion["data"], assertion["expected"])
@@ -27,7 +27,7 @@ for block in testdata["data"]:
             )
 
 
-@pytest.mark.parametrize("testcase", testcases, ids=lambda x: f"{x[1]}::{x[2]}::{x[3]}")
-def test_shared(testcase: Testcase):
-    for target_query, data, expected in testcase[0]:
+@pytest.mark.parametrize("case", cases, ids=lambda x: f"{x[1]}::{x[2]}::{x[3]}")
+def test_shared(case: Case):
+    for target_query, data, expected in case[0]:
         assert query(target_query, data) == expected
