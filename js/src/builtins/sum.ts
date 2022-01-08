c6ebce2ea7e8957212553ffdb50402cd6aa77878
Evin Sellin <evinism@gmail.com>
2022-01-08 10:24:53 -0800

    test and unify summing behavior

--- js/src/builtins/sum.ts
+++ js/src/builtins/sum.ts
@@ -3,7 +3,7 @@ import { arity, validateType } from "../util";
 
 const sum: BuiltinFunction = arity(1, (args, stack, exec) => {
   const arg: RuntimeValue = validateType("array", exec(args[0], stack));
-  return arg.reduce((a, b) => a + b, 0);
+  return arg.reduce((acc, cur) => acc + validateType("number", cur), 0);
 });
 
 export default sum;
