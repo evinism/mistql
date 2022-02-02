150af048fea390cba989d799fc433ef5697b3425
Adam Trilling <adamtrilling@gmail.com>
2022-02-02 15:51:39 -0500

    evaluate truthiness

--- rust/src/eval.rs
+++ rust/src/eval.rs
@@ -1,12 +1,26 @@
-use crate::parse::{Expression, Literal, Value};
+use crate::parse::{Expression, Literal, Operator, Value};
 use crate::Result;
 
 impl<'a> Expression<'a> {
     pub fn evaluate(&self, context: &serde_json::Value) -> Result<serde_json::Value> {
         match self {
             Self::Value(val) => val.evaluate(context),
-            Self::Monad { op: _, target } => target.evaluate(context),
+            Self::Monad {
+                op: Operator::Not,
+                target,
+            } => Ok((!is_truthy(target.evaluate(context)?)).into()),
         }
+    }
+}
+
+fn is_truthy(val: serde_json::Value) -> bool {
+    match val {
+        serde_json::Value::Null => false,
+        serde_json::Value::Number(num) => num.as_f64().unwrap() != 0.0,
+        serde_json::Value::Bool(boolean) => boolean,
+        serde_json::Value::String(string) => string.len() > 0,
+        serde_json::Value::Array(arr) => arr.len() > 0,
+        serde_json::Value::Object(obj) => obj.len() > 0,
     }
 }
 
