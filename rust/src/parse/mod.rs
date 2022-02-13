6bc74b0166d766a87e24fc2a29097ada158d823e
Adam Trilling <adamtrilling@gmail.com>
2022-02-12 22:23:06 -0500

    eval bools and numbers

--- rust/src/parse/mod.rs
+++ rust/src/parse/mod.rs
@@ -10,6 +10,9 @@ pub struct MistQLParser;
 #[derive(Debug)]
 pub enum Node {
     At,
+    Bool(bool),
+    Int(i32),
+    Float(f64),
 }
 
 pub fn parse_query(query: &str) -> Result<Node> {
@@ -20,12 +23,34 @@ pub fn parse_query(query: &str) -> Result<Node> {
             let root = pair.next().unwrap();
             match root.as_rule() {
                 Rule::at => Ok(Node::At),
+                Rule::bool => parse_bool(root.as_str()),
+                Rule::number => parse_number(root.as_str()),
                 _ => Err(Error::query(format!(
                     "unimplemented rule {:?}",
                     root.as_rule()
                 ))),
             }
         }
+    }
+}
+
+fn parse_bool(query: &str) -> Result<Node> {
+    match query.parse::<bool>() {
+        Ok(val) => Ok(Node::Bool(val)),
+        Err(_) => unreachable!("{} doesn't parse to bool", query),
+    }
+}
+
+fn parse_number(query: &str) -> Result<Node> {
+    match query.parse::<f64>() {
+        Ok(val) => {
+            if val.fract() == 0.0 {
+                Ok(Node::Int(val as i32))
+            } else {
+                Ok(Node::Float(val))
+            }
+        }
+        Err(_) => unreachable!("{} doesn't parse to number", query),
     }
 }
 
