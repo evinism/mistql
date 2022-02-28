f30fa0773b8008d70d80731d93a27a0e323a6fd5
Adam Trilling <adamtrilling@gmail.com>
2022-02-27 22:29:06 -0500

    treat simple literals as serde_json values in parser

--- rust/src/eval/mod.rs
+++ rust/src/eval/mod.rs
@@ -5,9 +5,7 @@ impl Node {
     pub fn evaluate(&self, context: &serde_json::Value) -> Result<serde_json::Value> {
         match self {
             Self::At => Ok(context.clone()),
-            Self::Bool(val) => Ok(val.clone().into()),
-            Self::Int(val) => Ok(val.clone().into()),
-            Self::Float(val) => Ok(val.clone().into()),
+            Self::Value(val) => Ok(val.clone().into()),
         }
     }
 }
--- rust/src/parse/mod.rs
+++ rust/src/parse/mod.rs
@@ -1,6 +1,6 @@
 use crate::error::{Error, Result};
 
-use pest::Parser;
+use pest::{iterators::Pair, Parser};
 use pest_derive::Parser;
 
 #[derive(Parser)]
@@ -10,9 +10,7 @@ pub struct MistQLParser;
 #[derive(Debug)]
 pub enum Node {
     At,
-    Bool(bool),
-    Int(i32),
-    Float(f64),
+    Value(serde_json::Value),
 }
 
 pub fn parse_query(query: &str) -> Result<Node> {
@@ -23,8 +21,7 @@ pub fn parse_query(query: &str) -> Result<Node> {
             let root = pair.next().unwrap();
             match root.as_rule() {
                 Rule::at => Ok(Node::At),
-                Rule::bool => parse_bool(root.as_str()),
-                Rule::number => parse_number(root.as_str()),
+                Rule::bool | Rule::number | Rule::string | Rule::null => parse_value(root),
                 _ => Err(Error::query(format!(
                     "unimplemented rule {:?}",
                     root.as_rule()
@@ -34,23 +31,10 @@ pub fn parse_query(query: &str) -> Result<Node> {
     }
 }
 
-fn parse_bool(query: &str) -> Result<Node> {
-    match query.parse::<bool>() {
-        Ok(val) => Ok(Node::Bool(val)),
-        Err(_) => unreachable!("{} doesn't parse to bool", query),
-    }
-}
-
-fn parse_number(query: &str) -> Result<Node> {
-    match query.parse::<f64>() {
-        Ok(val) => {
-            if val.fract() == 0.0 {
-                Ok(Node::Int(val as i32))
-            } else {
-                Ok(Node::Float(val))
-            }
-        }
-        Err(_) => unreachable!("{} doesn't parse to number", query),
+fn parse_value(query: Pair<Rule>) -> Result<Node> {
+    match serde_json::from_str(query.as_str()) {
+        Ok(val) => Ok(Node::Value(val)),
+        Err(_) => Err(Error::query(format!("unparseable value {:?}", query))),
     }
 }
 
