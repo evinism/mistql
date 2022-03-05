388943fe82403f3c4e1bcb543b9c94fcdb118631
Adam Trilling <adamtrilling@gmail.com>
2022-03-05 11:20:25 -0500

    evaluate arrays

---
+++ rust/src/eval/expr.rs
@@ -0,0 +1,16 @@
+use pest::iterators::Pair;
+
+use crate::eval::{array, value};
+use crate::{Error, Result, Rule};
+
+pub fn eval(pair: Pair<Rule>, context: &serde_json::Value) -> Result<serde_json::Value> {
+    match pair.as_rule() {
+        Rule::at => Ok(context.clone()),
+        Rule::bool | Rule::number | Rule::string | Rule::null => value::eval(pair, &context),
+        Rule::array => array::eval(pair, &context),
+        _ => Err(Error::unimplemented(format!(
+            "unimplemented rule {:?}",
+            pair.as_rule()
+        ))),
+    }
+}
