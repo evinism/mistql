66563b8f80c48a3b897da2febc7c1884d906a6c6
Adam Trilling <adamtrilling@gmail.com>
2022-03-08 10:54:33 -0500

    correct behavior of and operator

--- rust/src/eval/infix/boolean.rs
+++ rust/src/eval/infix/boolean.rs
@@ -2,9 +2,74 @@ use crate::eval::prefix::truthiness;
 use crate::Result;
 
 pub fn and(left: serde_json::Value, right: serde_json::Value) -> Result<serde_json::Value> {
-    Ok((truthiness(left) && truthiness(right)).into())
+    match (truthiness(&left), truthiness(&right)) {
+        (false, _) => Ok(left),
+        (true, false) => Ok(right),
+        (true, true) => Ok(right),
+    }
 }
 
 pub fn or(left: serde_json::Value, right: serde_json::Value) -> Result<serde_json::Value> {
-    Ok((truthiness(left) || truthiness(right)).into())
+    Ok((truthiness(&left) || truthiness(&right)).into())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn and_on_booleans() {
+        let result = crate::query("true && true".to_string(), "null".to_string()).unwrap();
+        assert_eq!(result, serde_json::Value::Bool(true));
+
+        let result = crate::query("true && false".to_string(), "null".to_string()).unwrap();
+        assert_eq!(result, serde_json::Value::Bool(false));
+
+        let result = crate::query("false && true".to_string(), "null".to_string()).unwrap();
+        assert_eq!(result, serde_json::Value::Bool(false));
+
+        let result = crate::query("false && false".to_string(), "null".to_string()).unwrap();
+        assert_eq!(result, serde_json::Value::Bool(false));
+    }
+
+    #[test]
+    fn and_on_truthy_values() {
+        let result = crate::query("1 && 1".to_string(), "null".to_string()).unwrap();
+        assert_eq!(result, serde_json::Value::from(1));
+
+        let result = crate::query("1 && 0".to_string(), "null".to_string()).unwrap();
+        assert_eq!(result, serde_json::Value::from(0));
+
+        let result = crate::query("\"a\" && \"\"".to_string(), "null".to_string()).unwrap();
+        assert_eq!(result, serde_json::Value::from(""));
+
+        let result = crate::query("\"\" && \"a\"".to_string(), "null".to_string()).unwrap();
+        assert_eq!(result, serde_json::Value::from(""));
+
+        let result = crate::query("\"a\" && \"b\"".to_string(), "null".to_string()).unwrap();
+        assert_eq!(result, serde_json::Value::from("b"));
+
+        let result = crate::query("[1] && []".to_string(), "null".to_string()).unwrap();
+        assert_eq!(result, serde_json::Value::Array(vec![]));
+
+        let result = crate::query("[1] && [2]".to_string(), "null".to_string()).unwrap();
+        let expected = serde_json::Value::Array(vec![(2 as u32).into()]);
+        assert_eq!(result, expected);
+
+        let result = crate::query("[] && [2]".to_string(), "null".to_string()).unwrap();
+        assert_eq!(result, serde_json::Value::Array(vec![]));
+    }
+
+    #[test]
+    fn and_on_mixed_types() {
+        let result = crate::query("null && 0".to_string(), "null".to_string()).unwrap();
+        assert_eq!(result, serde_json::Value::Null);
+
+        let result = crate::query("null && 1".to_string(), "null".to_string()).unwrap();
+        assert_eq!(result, serde_json::Value::Null);
+
+        let result = crate::query("0 && null".to_string(), "null".to_string()).unwrap();
+        assert_eq!(result, serde_json::Value::from(0));
+
+        let result = crate::query("1 && null".to_string(), "null".to_string()).unwrap();
+        assert_eq!(result, serde_json::Value::Null);
+    }
 }
