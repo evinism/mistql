4fc6599b509654dbab5abac2a8b1d5159176e9fd
Evin Sellin <evinism@gmail.com>
2022-03-24 23:23:01 -0700

    query language to common expression language

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
   module.exports = {
     title: "MistQL",
     tagline:
-      "A miniature embeddable language for performing computations on JSON-like structures",
+      "A common expression language for JSON-like structures",
     url: "https://mistql.com",
     baseUrl: "/",
     onBrokenLinks: "throw",
