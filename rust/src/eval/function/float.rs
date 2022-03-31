3a02aedf6dfe7c6db57fe3e9f3db1c27d6a17a07
Adam Trilling <adamtrilling@gmail.com>
2022-03-30 22:39:00 -0400

    float function (148)

--- rust/src/eval/function/float.rs
+++ rust/src/eval/function/float.rs
@@ -1,20 +1,18 @@
+use crate::eval::Value;
 use crate::{Error, Result};
 
-pub fn float(args: Vec<serde_json::Value>) -> Result<serde_json::Value> {
+pub fn float(args: Vec<Value>) -> Result<Value> {
     if let Some(val) = args.get(0) {
         match val {
-            // serde_json::from_str(pair.as_str());
-            serde_json::Value::Number(num) => match num.as_f64() {
-                Some(float_num) => Ok(serde_json::Value::from(float_num)),
-                None => Err(Error::eval(format!("invalid number {}", num))),
-            },
-            serde_json::Value::String(string) => match string.trim().parse::<f64>() {
-                Ok(val) => Ok(val.into()),
+            Value::Float(_) => Ok(val.clone()),
+            Value::Int(num) => Ok(Value::Float(*num as f64)),
+            Value::String(string) => match string.trim().parse::<f64>() {
+                Ok(val) => Ok(Value::Float(val)),
                 Err(err) => Err(Error::eval(err.to_string())),
             },
-            serde_json::Value::Bool(true) => Ok(serde_json::Value::from(1.0)),
-            serde_json::Value::Bool(false) => Ok(serde_json::Value::from(0.0)),
-            serde_json::Value::Null => Ok(serde_json::Value::from(0.0)),
+            Value::Boolean(true) => Ok(Value::Float(1.0)),
+            Value::Boolean(false) => Ok(Value::Float(0.0)),
+            Value::Null => Ok(Value::Float(0.0)),
             _ => Err(Error::eval("argument does not cast to float".to_string())),
         }
     } else {
