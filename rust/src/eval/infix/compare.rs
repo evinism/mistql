865ca00d31a3ab6b46e84287a31b19cf0a058461
Adam Trilling <adamtrilling@gmail.com>
2022-03-30 20:55:32 -0400

    compare operators (273)

--- rust/src/eval/infix/compare.rs
+++ rust/src/eval/infix/compare.rs
@@ -1,57 +1,62 @@
+use crate::eval::Value;
 use crate::{Error, Result};
 
-pub fn gte(left: serde_json::Value, right: serde_json::Value) -> Result<serde_json::Value> {
-    if let (Some(l), Some(r)) = (left.as_f64(), right.as_f64()) {
-        Ok((l >= r).into())
-    } else if let (Some(l), Some(r)) = (left.as_str(), right.as_str()) {
-        Ok((l >= r).into())
-    } else {
-        Err(Error::eval(
+pub fn gte(left: Value, right: Value) -> Result<Value> {
+    match (left, right) {
+        (Value::Int(l), Value::Int(r)) => Ok(Value::Boolean(l >= r)),
+        (Value::Int(l), Value::Float(r)) => Ok(Value::Boolean(l as f64 >= r)),
+        (Value::Float(l), Value::Int(r)) => Ok(Value::Boolean(l >= r as f64)),
+        (Value::Float(l), Value::Float(r)) => Ok(Value::Boolean(l >= r)),
+        (Value::String(l), Value::String(r)) => Ok(Value::Boolean(l >= r)),
+        _ => Err(Error::eval(
             "can only compare numbers or strings".to_string(),
-        ))
+        )),
     }
 }
 
-pub fn gt(left: serde_json::Value, right: serde_json::Value) -> Result<serde_json::Value> {
-    if let (Some(l), Some(r)) = (left.as_f64(), right.as_f64()) {
-        Ok((l > r).into())
-    } else if let (Some(l), Some(r)) = (left.as_str(), right.as_str()) {
-        Ok((l > r).into())
-    } else {
-        Err(Error::eval(
+pub fn gt(left: Value, right: Value) -> Result<Value> {
+    match (left, right) {
+        (Value::Int(l), Value::Int(r)) => Ok(Value::Boolean(l > r)),
+        (Value::Int(l), Value::Float(r)) => Ok(Value::Boolean(l as f64 > r)),
+        (Value::Float(l), Value::Int(r)) => Ok(Value::Boolean(l > r as f64)),
+        (Value::Float(l), Value::Float(r)) => Ok(Value::Boolean(l > r)),
+        (Value::String(l), Value::String(r)) => Ok(Value::Boolean(l > r)),
+        _ => Err(Error::eval(
             "can only compare numbers or strings".to_string(),
-        ))
+        )),
     }
 }
 
-pub fn lte(left: serde_json::Value, right: serde_json::Value) -> Result<serde_json::Value> {
-    if let (Some(l), Some(r)) = (left.as_f64(), right.as_f64()) {
-        Ok((l <= r).into())
-    } else if let (Some(l), Some(r)) = (left.as_str(), right.as_str()) {
-        Ok((l <= r).into())
-    } else {
-        Err(Error::eval(
+pub fn lte(left: Value, right: Value) -> Result<Value> {
+    match (left, right) {
+        (Value::Int(l), Value::Int(r)) => Ok(Value::Boolean(l <= r)),
+        (Value::Int(l), Value::Float(r)) => Ok(Value::Boolean(l as f64 <= r)),
+        (Value::Float(l), Value::Int(r)) => Ok(Value::Boolean(l <= r as f64)),
+        (Value::Float(l), Value::Float(r)) => Ok(Value::Boolean(l <= r)),
+        (Value::String(l), Value::String(r)) => Ok(Value::Boolean(l <= r)),
+        _ => Err(Error::eval(
             "can only compare numbers or strings".to_string(),
-        ))
+        )),
     }
 }
 
-pub fn lt(left: serde_json::Value, right: serde_json::Value) -> Result<serde_json::Value> {
-    if let (Some(l), Some(r)) = (left.as_f64(), right.as_f64()) {
-        Ok((l < r).into())
-    } else if let (Some(l), Some(r)) = (left.as_str(), right.as_str()) {
-        Ok((l < r).into())
-    } else {
-        Err(Error::eval(
+pub fn lt(left: Value, right: Value) -> Result<Value> {
+    match (left, right) {
+        (Value::Int(l), Value::Int(r)) => Ok(Value::Boolean(l < r)),
+        (Value::Int(l), Value::Float(r)) => Ok(Value::Boolean((l as f64) < r)),
+        (Value::Float(l), Value::Int(r)) => Ok(Value::Boolean(l < r as f64)),
+        (Value::Float(l), Value::Float(r)) => Ok(Value::Boolean(l < r)),
+        (Value::String(l), Value::String(r)) => Ok(Value::Boolean(l < r)),
+        _ => Err(Error::eval(
             "can only compare numbers or strings".to_string(),
-        ))
+        )),
     }
 }
 
-pub fn eq(left: serde_json::Value, right: serde_json::Value) -> Result<serde_json::Value> {
-    Ok((left == right).into())
+pub fn eq(left: Value, right: Value) -> Result<Value> {
+    Ok(Value::Boolean(left == right))
 }
 
-pub fn ne(left: serde_json::Value, right: serde_json::Value) -> Result<serde_json::Value> {
-    Ok((left != right).into())
+pub fn ne(left: Value, right: Value) -> Result<Value> {
+    Ok(Value::Boolean(left != right))
 }
--- rust/src/eval/value.rs
+++ rust/src/eval/value.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum Value {
     Null,
     Boolean(bool),
