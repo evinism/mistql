d4789a53eccfae333ccde580f0fdb8debebcdea7
Adam Trilling <adamtrilling@gmail.com>
2022-04-01 20:44:01 -0400

    move each function to its own module

---
+++ rust/src/eval/function/string.rs
@@ -0,0 +1,24 @@
+use crate::{eval::Value, Error, Result};
+
+pub fn string(args: Vec<Value>) -> Result<Value> {
+    match (args.len(), args.get(0)) {
+        (1, Some(val)) => Ok(Value::String(val.to_string())),
+        (n, _) => Err(Error::eval(format!("count expected 1 argument, got {}", n))),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::string;
+    use crate::eval::Value;
+
+    #[test]
+    fn string_takes_one_arg() {
+        assert!(string(vec![Value::Int(1)]).is_ok());
+        assert!(string(vec![]).is_err());
+        assert!(string(vec![Value::Int(1), Value::Float(2.0)]).is_err());
+    }
+
+    // since this function delegates to the Display trait on value, unit
+    // tests appear there
+}
