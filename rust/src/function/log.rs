e3ba38607cd97183ea2eae236f195716b8cbf87c
Adam Trilling <adamtrilling@gmail.com>
2022-04-05 08:23:08 -0400

    handle implicit at in log (265)

--- rust/src/function/log.rs
+++ rust/src/function/log.rs
@@ -1,19 +1,23 @@
 use crate::{expr, Error, Result, Rule, Value};
 use pest::iterators::Pairs;
 
-pub fn log(mut arg_itr: Pairs<Rule>, data: &Value, context: Option<Value>) -> Result<Value> {
-    match (arg_itr.next(), arg_itr.next()) {
-        (Some(arg), None) => match expr::eval(arg, data, context) {
+pub fn log(mut arg_itr: Pairs<Rule>, data: &Value, context_opt: Option<Value>) -> Result<Value> {
+    match (context_opt, arg_itr.next(), arg_itr.next()) {
+        (context, Some(arg), None) => match expr::eval(arg, data, context) {
             Ok(result) => {
                 dbg!(result.clone());
                 Ok(result)
             }
             Err(err) => Err(err),
         },
-        (None, _) => Err(Error::eval(
+        (Some(context), None, None) => {
+            dbg!(context.clone());
+            Ok(context)
+        }
+        (None, None, _) => Err(Error::eval(
             "log requires one argument (got zero)".to_string(),
         )),
-        (_, Some(_)) => Err(Error::eval(
+        (_, _, Some(_)) => Err(Error::eval(
             "log requires one argument (got >1)".to_string(),
         )),
     }
@@ -27,5 +31,21 @@ mod tests {
     fn log_takes_one_arg() {
         assert!(query_value("log 1".to_string(), serde_json::Value::Null).is_ok());
         assert!(query_value("log 1 2".to_string(), serde_json::Value::Null).is_err());
+    }
+
+    #[test]
+    fn log_uses_implicit_at() {
+        assert_eq!(
+            query_value("123 | log".to_string(), serde_json::Value::Null).unwrap(),
+            serde_json::Value::from(123)
+        );
+    }
+
+    #[test]
+    fn log_uses_explicit_at() {
+        assert_eq!(
+            query_value("123 | log @".to_string(), serde_json::Value::Null).unwrap(),
+            serde_json::Value::from(123)
+        );
     }
 }
