d8771803deb624282f512e125a758f1054945ae4
Adam Trilling <adamtrilling@gmail.com>
2022-04-05 09:04:19 -0400

    reimplement count with lazy args (260)

--- rust/src/function/count.rs
+++ rust/src/function/count.rs
@@ -1,46 +1,47 @@
-use crate::{Error, Result, Value};
+use crate::{expr, Error, Result, Rule, Value};
+use pest::iterators::Pairs;
 
-pub fn count(args: Vec<Value>) -> Result<Value> {
-    match (args.len(), args.get(0)) {
-        (1, Some(Value::Array(val))) => Ok(Value::Int(val.len() as i64)),
-        (1, Some(val)) => Err(Error::eval(format!(
+pub fn count(mut arg_itr: Pairs<Rule>, data: &Value, context_opt: Option<Value>) -> Result<Value> {
+    let arg = match (context_opt, arg_itr.next(), arg_itr.next()) {
+        (Some(val), None, None) => val,
+        (None, Some(val), None) => expr::eval(val, data, None)?,
+        _ => return Err(Error::eval("count requires one argument".to_string())),
+    };
+
+    match arg {
+        Value::Array(arr) => Ok(Value::Int(arr.len() as i64)),
+        _ => Err(Error::eval(format!(
             "argument to count must be an array (got {:?}",
-            val
+            arg
         ))),
-        (n, _) => Err(Error::eval(format!("count expected 1 argument, got {}", n))),
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::count;
-    use crate::Value;
+    use crate::query_value;
 
     #[test]
     fn count_takes_one_arg() {
-        assert!(count(vec![]).is_err());
-        assert!(count(vec![Value::Array(vec![])]).is_ok());
-        assert!(count(vec![Value::Array(vec![]), Value::Array(vec![])]).is_err());
+        assert!(query_value("count [1,2,3]".to_string(), serde_json::Value::Null).is_ok());
+        assert!(query_value("count [1,2,3] [4,5,6]".to_string(), serde_json::Value::Null).is_err());
     }
 
     #[test]
     fn count_arg_must_be_an_array() {
-        assert!(count(vec![Value::Int(1)]).is_err());
-        assert!(count(vec![Value::String("abc".to_string())]).is_err());
+        assert!(query_value("count 1".to_string(), serde_json::Value::Null).is_err());
+        assert!(query_value("count \"abc\"".to_string(), serde_json::Value::Null).is_err());
     }
 
     #[test]
     fn count_counts() {
-        assert_eq!(Value::Int(0), count(vec![Value::Array(vec![])]).unwrap());
-
         assert_eq!(
-            Value::Int(3),
-            count(vec![Value::Array(vec![
-                Value::Int(1),
-                Value::Int(2),
-                Value::Int(3)
-            ])])
-            .unwrap()
+            query_value("count []".to_string(), serde_json::Value::Null).unwrap(),
+            serde_json::Value::from(0)
+        );
+        assert_eq!(
+            query_value("count [1,2,3]".to_string(), serde_json::Value::Null).unwrap(),
+            serde_json::Value::from(3)
         );
     }
 }
