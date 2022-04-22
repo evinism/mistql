4444fe09e44265a4534dc69a067e0162482ba05f
Adam Trilling <adamtrilling@gmail.com>
2022-04-21 22:05:20 -0400

    summarize function (40)

--- rust/src/function/summarize.rs
+++ rust/src/function/summarize.rs
@@ -1,6 +1,7 @@
-use crate::{expr, Error, Result, Rule, Value};
+use crate::infix::arithmetic::sqrt;
+use crate::{expr, Error, Number, Result, Rule, Value};
 use pest::iterators::Pairs;
-// use std::collections::BTreeMap;
+use std::collections::BTreeMap;
 
 pub fn summarize(
     mut arg_itr: Pairs<Rule>,
@@ -14,14 +15,70 @@ pub fn summarize(
     };
 
     match arg {
-        Value::Array(_arr) => {
-            // let mut result: BTreeMap<String, Value> = BTreeMap::new();
+        Value::Array(arr) if arr.len() > 0 => {
+            let mut nums = arr
+                .into_iter()
+                .map(|elt| match elt {
+                    Value::Number(n) => Ok(n),
+                    _ => Err(Error::eval(
+                        "argument to summarize must be an array of Numbers".to_string(),
+                    )),
+                })
+                .collect::<Result<Vec<Number>>>()?;
+            nums.sort_unstable();
+            let mean = mean(&nums);
+            let variance = variance(&nums, &mean);
 
-            Err(Error::unimplemented("summarize function".to_string()))
+            let mut result = BTreeMap::new();
+            result.insert("max".to_string(), max(&nums));
+            result.insert("min".to_string(), min(&nums));
+            result.insert("mean".to_string(), Value::Number(mean.clone()));
+            result.insert("median".to_string(), median(&nums));
+            result.insert("variance".to_string(), variance.clone());
+            result.insert("stddev".to_string(), sqrt(variance)?);
+            Ok(Value::Object(result))
         }
         _ => Err(Error::eval(format!(
-            "argument to summarize must be an array (got {:?}",
+            "argument to summarize must be an non-empty array (got {:?}",
             arg
         ))),
     }
+}
+
+fn max(arr: &Vec<Number>) -> Value {
+    // safe unwrap - we know the vec has at least one element
+    Value::Number(arr.last().unwrap().clone())
+}
+
+fn min(arr: &Vec<Number>) -> Value {
+    // safe unwrap - we know the vec has at least one element
+    Value::Number(arr.first().unwrap().clone())
+}
+
+fn mean(arr: &Vec<Number>) -> Number {
+    let sum = arr.iter().fold(Number::Int(0), |acc, x| acc + x.clone());
+    sum / Number::Int(arr.len() as i64)
+}
+
+fn median(arr: &Vec<Number>) -> Value {
+    let res = match arr.len() {
+        even if even % 2 == 0 => {
+            let low = arr[even / 2].clone();
+            let high = arr[(even / 2) + 1].clone();
+            (low + high) / Number::Int(2)
+        }
+        odd => arr[odd / 2].clone(),
+    };
+    Value::Number(res)
+}
+
+fn variance(arr: &Vec<Number>, mean: &Number) -> Value {
+    let sum_of_diffs = arr
+        .iter()
+        .map(|val| {
+            let diff = mean.clone() - val.clone();
+            diff.clone() * diff
+        })
+        .fold(Number::Float(0.0), |acc, x| acc + x);
+    Value::Number(sum_of_diffs / Number::Int(arr.len() as i64 - 1))
 }
