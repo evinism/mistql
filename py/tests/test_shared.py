74366098c8c63d964e2595bc2c1b0696bc0541e5
Evin Sellin <evinism@gmail.com>
2022-05-10 16:57:54 -0700

    add test skipping to js and python

--- py/tests/test_shared.py
+++ py/tests/test_shared.py
@@ -1,5 +1,5 @@
 import json
-from typing import Any, List, Tuple
+from typing import Any, List, Optional, Tuple
 
 import pytest
 from mistql import query
@@ -8,30 +8,44 @@ with open("shared/testdata.json", "rb") as f:
     testdata = json.load(f)
 
 
-Case = Tuple[List[Tuple[str, Any, Any]], str, str, str]
-cases: List[Tuple[List[Tuple[str, Any, Any]], str, str, str]] = []
+Case = Tuple[List[Tuple[str, Any, Any]], str, str, str, Optional[str]]
+non_skipped_cases: List[Case] = []
+skipped_cases: List[Case] = []
+
+
+SELF_LANG_ID = "py"
+
 
 for block in testdata["data"]:
     for innerblock in block["cases"]:
         for test in innerblock["cases"]:
-            cases.append(
+            if SELF_LANG_ID in test.get("skip", []):
+                target = skipped_cases
+            else:
+                target = non_skipped_cases
+            target.append(
                 (
                     [
                         (
                             assertion["query"],
                             assertion["data"],
                             assertion.get("expected"),
-                            assertion.get("throws")
+                            assertion.get("throws"),
                         ) for assertion in test["assertions"]
                     ],
                     block["describe"],
                     innerblock["describe"],
                     test["it"],
+                    test.get("skip")
                 )
             )
 
 
-@pytest.mark.parametrize("case", cases, ids=lambda x: f"{x[1]}::{x[2]}::{x[3]}")
+def get_test_id_for_case(case: Case) -> str:
+    return f"{case[1]}::{case[2]}::{case[3]}"
+
+
+@pytest.mark.parametrize("case", non_skipped_cases, ids=get_test_id_for_case)
 def test_shared(case: Case):
     for target_query, data, expected, throws in case[0]:
         if throws:
@@ -39,3 +53,10 @@ def test_shared(case: Case):
                 query(target_query, data)
         else:
             assert query(target_query, data) == expected
+
+
+if len(skipped_cases) > 0:
+    @pytest.mark.skip
+    @pytest.mark.parametrize("case", skipped_cases, ids=get_test_id_for_case)
+    def test_shared_skipped(case: Case):
+        pass
