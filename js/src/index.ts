91fec2017fdb9cab32c11bc4c521df80cbf40977
Evin Sellin <evinism@gmail.com>
2022-12-19 16:44:11 -0800

    Add interface for supplying custom functions to MistQL

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,8 +1,20 @@
-import { execute } from "./executor";
-import { parseOrThrow } from "./parser";
+import {
+  defaultInstance as DI,
+  MistQLInstance as MQI,
+  MistQLOptions as MQO,
+} from "./instance";
+export {
+  FunctionValue,
+  RuntimeValue,
+  RuntimeValueType,
+  FunctionClosure,
+  ASTExpression,
+} from "./types";
 
-export const query = (query: string, data: any) => {
-  return execute(parseOrThrow(query), data);
-};
+export const MistQLInstance = MQI;
+export const defaultInstance = DI;
+export const query = DI.query;
 
-export default { query };
+export type MistQLOptions = MQO;
+
+export default { query, defaultInstance, MistQLInstance };
