3c7a85e56a4cfa34e9e496d5c2cd05ca075e7abd
Evin Sellin <evinism@gmail.com>
2022-12-19 21:50:54 -0800

    first pass of custom functions in python

--- py/mistql/query.py
+++ py/mistql/query.py
@@ -1,11 +1,8 @@
 from typing import Any
 
-from .execute import execute_outer
-from .gardenwall import input_garden_wall, output_garden_wall
-from .parse import parse
+from .instance import default_instance
 
-
-def query(query: str, raw_data: Any) -> Any:
+def query(query: str, data: Any) -> Any:
     """
     Executes a query on a given data.
 
@@ -13,8 +10,4 @@ def query(query: str, raw_data: Any) -> Any:
     :param data: The data to query.
     :return: The result of the query.
     """
-    ast = parse(query)
-    data = input_garden_wall(raw_data)
-    result = execute_outer(ast, data)
-    return_value = output_garden_wall(result)
-    return return_value
+    return default_instance.query(query, data)
