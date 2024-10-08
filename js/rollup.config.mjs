59ed563257e2cb6378209a51cfa613fbb0f5dce5
Evin Sellin <evinism@gmail.com>
2024-10-08 01:25:03 -0700

    update dependencies, switch to npm

--- js/rollup.config.mjs
+++ js/rollup.config.mjs
@@ -1,7 +1,7 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import analyze from "rollup-plugin-analyzer";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 export default {
   input: "dist/esm/nodefault.js",
