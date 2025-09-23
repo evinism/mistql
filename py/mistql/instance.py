83a6f246e939237cc471b6d690a7c3069de8f436
Evin Sellin <evinism@gmail.com>
2025-09-22 23:28:30 -0700

    add lru to the parser in python

--- py/mistql/instance.py
+++ py/mistql/instance.py
@@ -1,4 +1,5 @@
 from typing import Dict, Union, Callable, Optional, Any
+from functools import lru_cache
 
 from .execute import execute_outer
 from .runtime_value import RuntimeValue
@@ -11,12 +12,18 @@ ExtrasDict = Dict[str, Union[RuntimeValue, Callable]]
 
 class MistQLInstance:
     extras: ExtrasDict
+    parse_lru_cache_size: int
+    _cached_parse: Callable[[str], Any]
 
-    def __init__(self, extras: Optional[ExtrasDict] = None):
+    def __init__(
+        self, extras: Optional[ExtrasDict] = None, parse_lru_cache_size: int = 4
+    ):
         self.extras = extras or {}
+        self.parse_lru_cache_size = parse_lru_cache_size
+        self._cached_parse = lru_cache(maxsize=parse_lru_cache_size)(parse)
 
     def query(self, query: str, data: Any):
-        ast = parse(query)
+        ast = self._cached_parse(query)
         data = input_garden_wall(data)
         result = execute_outer(ast, data, self.extras)
         return_value = output_garden_wall(result)
