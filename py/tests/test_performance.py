2148e245067b04568b2954cdfff330e59124b200
Evin Sellin <evinism@gmail.com>
2025-09-23 23:41:01 -0700

    profiling and snakeviz

--- py/tests/test_performance.py
+++ py/tests/test_performance.py
@@ -2,14 +2,25 @@ from mistql import MistQLInstance
 import json
 import os
 import time
+import cProfile
+import pstats
+import io
 
 
 def test_performance():
     with open(os.path.join(os.path.dirname(__file__), "..", "shared", "data", "nobel-prizes.json"), "r") as f:
         nobel_prizes = json.load(f)
-    mq = MistQLInstance(lazy=False)
-    start_time = time.time()
-    for i in range(5000):
+    mq = MistQLInstance(lazy=True)
+    
+    # Profile the performance test
+    profiler = cProfile.Profile()
+    profiler.enable()
+    
+    for i in range(1000):
       mq.query("count @.prizes", nobel_prizes)
-    end_time = time.time()
-    print(f"Time taken: {end_time - start_time} seconds")
+    
+    profiler.disable()
+    # Print profiling results
+    s = io.StringIO()
+    ps = pstats.Stats(profiler, stream=s).sort_stats('cumulative')
+    ps.dump_stats("profile.pstats")
